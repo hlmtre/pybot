5737b81f20a5485adb072a79a4ed9249a2426fdc
Matt Weller <hellmitre@gmail.com>
2012-03-29 14:51:48 -0700

    whee and he forks

--- bot.py
+++ bot.py
@@ -15,89 +15,62 @@ NICK = 'ohai'
 IDENT = 'mypy'
 REALNAME = 's1ash'
 OWNER = 'hlmtre'
-CHANNELINIT = ['#hhorg', '#bf3', '#bots']
+CHANNELINIT = ['#bots', '#hhorg', '#bf3', '#cslug']
 readbuffer = ''
 DEBUG = False
+CONNECTED = False
 
 kcount = defaultdict(int)
-
-def parsemsg(line):
-	value = 0
-
-	# grab first occurrence of !
-	# fixes array index error on lines which contained more than one !
-	user = line[1:].split('!', 1)
-	username = user[0]
-	phrase = user[1:]
-	p = phrase[-1]
-# same deal here
-	q = p.split(':', 1)
-#	print q
-	p = q[-1]
-	c = p[0]
-	if c  ==  ">":
-		if username not in kcount:
-			kcount[username] = 1
-		else:
-			kcount[username] +=  1
-		if kcount[username] % 3  ==  0:
-			value = 1
-	else: 
-		value = 0
-# end of case/switch
-
-
-# logging stuff
-	logfile = open("./logfile.txt","a")
-	date = str(strftime("%Y-%m-%d %H:%M:%S"))
-	logfile.write("at " + date + ": ")
-	logfile.write(username + " greentexted.\n")#(str(kcount)+'\n')
-	logfile.write(username + " has greentexted " + str(kcount[username]) + " times since i've been running\n")
-	logfile.close()
-	return (value,username)
-
-# end of parsemsg
 
 ## MAIN
 
 if __name__ == "__main__":
+	pid = os.fork()
+	if pid > 0:
+		print "exiting..."
+		os._exit(0)
+	elif pid == 0:
+# we are the child
+		print os.getpid()
 # connect to server
-	s = socket.socket()
-	s.connect((HOST, PORT))
-	s.send('NICK '+NICK+'\n')
-	s.send('USER '+IDENT+ ' 8 ' + ' bla : '+REALNAME+'\n')
+		s = socket.socket()
+		s.connect((HOST, PORT))
+		s.send('NICK '+NICK+'\n')
+		s.send('USER '+IDENT+ ' 8 ' + ' bla : '+REALNAME+'\n')
 
 # infinite loop to keep parsing lines
-	while 1:
-		line = s.recv(500)
-		line_array = line.split()
+		while 1:
+			line = s.recv(500)
+			if line.find('PRIVMSG') !=  -1:
+				if CONNECTED == False:
+					for chan in CHANNELINIT:
+						s.send('JOIN '+chan+'\n')
+						CONNECTED = True
+				line_array = line.split()
 
-		ping_response_line = line.split()
-		if (ping_response_line[0]  ==  'PING'):
-			s.send('PONG ' + ping_response_line[1] + '\n')
-			date = str(strftime("%Y-%m-%d %H:%M:%S"))
-			print "responding to ping at " + date
+				ping_response_line = line.split()
+				if (ping_response_line[0]  ==  'PING'):
+					s.send('PONG ' + ping_response_line[1] + '\n')
+					date = str(strftime("%Y-%m-%d %H:%M:%S"))
+					print "responding to ping at " + date
 
-		elif "PRIVMSG" in line:
-			user_and_mask = line_array[0]
-			usr = user_and_mask.split("!", 1)[0]
-			channel = line_array[2]
-			message = line_array[3]
-			if "ohai" in line and "hello" in line:
-				s.send('PRIVMSG ' + channel + ' well hello to you too ' + usr + '\n')
+				elif "PRIVMSG" in line:
+					user_and_mask = line_array[0]
+					usr = user_and_mask.split("!", 1)[0]
+					usr = usr.split(":", 1)[1]
+					channel = line_array[2]
+					message = line_array[3]
+					message = message.split(":", 1)[1]
+					if "ohai" in line and "hello" in line:
+						s.send('PRIVMSG ' + channel + ' well hello to you too ' + usr + '\n')
+					if message.startswith(">"):
+						if usr not in kcount:
+							kcount[usr] = 1
+						else:
+							kcount[usr] += 1
+						if kcount[usr] % 3 == 0:
+							s.send('PRIVMSG ' + channel + ' ' + usr +': >implying you\'re greentexting\n')
 
-		if line.find('PRIVMSG') !=  -1:
-			for chan in CHANNELINIT:
-				s.send('JOIN '+chan+'\n')
-
-			line = line.rstrip()
-# grab message to channel
-			msg = line.split(":", 2)[2]
-			mybool = parsemsg(line)
-			code = mybool[0]
-			myusername = mybool[1]
-			if DEBUG:
-				print user_and_mask + " said to " + channel + " " + msg
-			c = int(code)
-			if c > 0:
-				s.send('PRIVMSG ' + chan + ' ' + myusername + ': :>implying you\'re greentexting' +'\n')
+					if DEBUG:
+						print user_and_mask + " said to " + channel + " \"" + msg + "\""
+						print kcount
