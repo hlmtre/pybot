0c17a68d73b1a14b6385ffbdb008b3cfc222cd57
Matt Weller <hellmitre@gmail.com>
2013-03-15 02:20:49 -0700

    fix stupid handle idea in db.py

--- db.py
+++ db.py
@@ -2,8 +2,14 @@ import MySQLdb as mdb
 from datetime import datetime,timedelta
 class DB:
 	age = datetime.now()
-	con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
-	cur = con.cursor()
+
+	def _open(self):
+		self.con = mdb.connect("localhost","pybot","1q2w3e4r","pybot")
+		self.cur = con.cursor()
+	
+	def _close(self):
+		self.con = None
+		self.cur.close()
 
 	# should prevent mysql has gone away errors.. ideally
 	def _handle(self):
@@ -16,29 +22,27 @@ class DB:
 			self.cur = self.con.cursor()
 
 	def insert(self, where, which, what):
-		global cur
 		self.cur.execute("""INSERT INTO %s (%s) VALUES (%s)""",(where, which, what)) 
 
 	def updateSeen(self,who,statement,event):
-		self._handle()
-		global cur
+		self._open()
 		#print "executing REPLACE INTO seen (user_name, statement, event) VALUES ( " + str(who) + " " + str(statement) + " " + str(event) + ")"
 		self.cur.execute("REPLACE INTO seen (user_name, statement, event) VALUES (%s, %s, %s)", (who, statement, event))
+		self._close()
 
 	def getSeen(self, who):
-		self._handle()
-		global cur
+		self._open()
 		if who != "":
 			self.cur.execute("SELECT user_name, date, statement, event FROM seen WHERE user_name = %s", who)
 			data = self.cur.fetchone()
 			return data;
+			self._close()
 		else:
+			self._close()
 			return None 
 
 	def _insertImg(self, user, url):
-		self._handle()
-		global cur
-		global con
+		self._open()
 		if user == "" or user == None:
 			user = "nobody"
 		try:
@@ -47,26 +51,28 @@ class DB:
 		except:
 			self.con.rollback()
 
+		self._close()
+
 	def _getImgs(self):
-		self._handle()
-		global cur
-		global con
+		self._open()
 		try:
 			self.cur.execute("""SELECT * FROM img ORDER BY time""")
 			data = self.cur.fetchall()
+			self._close()
 		except:
+			self._close()
 			return None
 
 		return data
 
 	def _isAdmin(self, username):
-		self._handle()
-		global cur
-		global con
+		self._open()
 		try:
 			self.cur.execute("""SELECT * FROM admins WHERE username = %s""",(username))
 			data = self.cur.fetchall()
+			self._close()
 		except:
+			self._close()
 			return None
 
 		return data
