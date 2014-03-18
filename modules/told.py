b2d18e4bd4a2854790a7e74aa54cfb4bf246f490
Matt Weller <hellmitre@gmail.com>
2014-03-18 08:22:21 -0700

    more tab fixes for told

--- modules/told.py
+++ modules/told.py
@@ -18,55 +18,54 @@ class Told:
 
     def get_told_status(self):
         """Randomly selects and returns a string with a "told" status."""
-	status = ["FUCKING TOLD",
-		  "CASH4TOLD.COM",
-		  "KNIGHTS OF THE TOLD REPUBLIC",
-		  "STONE TOLD STEVE AUSTIN",
-		  "CURE FOR THE COMMON TOLD",
-		  "BEN TOLDS FIVE",
-		  "THE 40 YEAR TOLD VIRGIN",
-		  "007: TOLDENEYE",
-		  "TEXAS TOLD'EM",
-		  "AUSTIN POWERS IN TOLDMEMBER",
-		  "PTERODACTOLD",
-		  "NO COUNTRY FOR TOLD MEN",
-		  "24 CARAT TOLD RING",
-		  "ONLY SHOOTING STARS BREAK THE TOLD",
-		  "GOING ONCE...GOING TWICE...TOLD!",
-		  "GARY TOLDMAN",
-		  "TOLD SPICE",
-		  "TOLD STONE CREAMERY",
-		  "BABY IT'S TOLD OUTSIDE",
-		  "POKEMON TOLD AND SILVER",
-		  "TOLD YELLER",
-		  "EL DORADO: THE LOST CITY OF TOLD",
-		  "TOLDPLAY",
-		  "BATMAN: THE BRAVE AND THE TOLD",
-		  "DANNY DEVITOLD",
-		  "FOR WHOM THE BELL TOLDS",
-		  "CAN'T TEACH A TOLD DOG NEW TRICKS",
-		  "I AIN'T SAYING SHE A TOLD DIGGER",
-		  "THE TOLDEN COMPASS",
-		  "TOLDIER OF FORTUNE",
-		  "TOLDING CHAIR",
-		  "TOLDEN AXE",
-		  "TOLD MACDONALD HAD A FARM",
-		  "TOLDEN TOLDIES: HITS FROM THE 50'S, 60'S, AND 70'S",
-		  "BATTLETOLDS",
-		  "YE TOLDE PUB",
-		  "TOLDEN CAULFIELD",
-		  "THE TOLD MAN AND THE SEA",
-		  "TOLD MEDAL WINNER IN THE WINTER OLYMPICS",
-                  "POT OF TOLD AT THE END OF THE RAINBOW"
-		  ]
-	return "[X] " + random.choice(status)
+        status = ["FUCKING TOLD",
+            "CASH4TOLD.COM",
+            "KNIGHTS OF THE TOLD REPUBLIC",
+            "STONE TOLD STEVE AUSTIN",
+            "CURE FOR THE COMMON TOLD",
+            "BEN TOLDS FIVE",
+            "THE 40 YEAR TOLD VIRGIN",
+            "007: TOLDENEYE",
+            "TEXAS TOLD'EM",
+            "AUSTIN POWERS IN TOLDMEMBER",
+            "PTERODACTOLD",
+            "NO COUNTRY FOR TOLD MEN",
+            "24 CARAT TOLD RING",
+            "ONLY SHOOTING STARS BREAK THE TOLD",
+            "GOING ONCE...GOING TWICE...TOLD!",
+            "GARY TOLDMAN",
+            "TOLD SPICE",
+            "TOLD STONE CREAMERY",
+            "BABY IT'S TOLD OUTSIDE",
+            "POKEMON TOLD AND SILVER",
+            "TOLD YELLER",
+            "EL DORADO: THE LOST CITY OF TOLD",
+            "TOLDPLAY",
+            "BATMAN: THE BRAVE AND THE TOLD",
+            "DANNY DEVITOLD",
+            "FOR WHOM THE BELL TOLDS",
+            "CAN'T TEACH A TOLD DOG NEW TRICKS",
+            "I AIN'T SAYING SHE A TOLD DIGGER",
+            "THE TOLDEN COMPASS",
+            "TOLDIER OF FORTUNE",
+            "TOLDING CHAIR",
+            "TOLDEN AXE",
+            "TOLD MACDONALD HAD A FARM",
+            "TOLDEN TOLDIES: HITS FROM THE 50'S, 60'S, AND 70'S",
+            "BATTLETOLDS",
+            "YE TOLDE PUB",
+            "TOLDEN CAULFIELD",
+            "THE TOLD MAN AND THE SEA",
+            "TOLD MEDAL WINNER IN THE WINTER OLYMPICS",
+                        "POT OF TOLD AT THE END OF THE RAINBOW"
+            ]
+        return "[X] " + random.choice(status)
 
     def handle(self, event):
         _z = str.split(event.msg, None, 1)
-	#print "Debugging: Reached stage 1 of handle()"
         told_status = self.get_told_status()
         try:
-	    self.printer("PRIVMSG " + event.channel + ' :' + _z[1] + '\'s told status: ' + told_status + '\n')
+            self.printer("PRIVMSG " + event.channel + ' :' + _z[1] + '\'s told status: ' + told_status + '\n')
         except IndexError:
             self.printer("PRIVMSG " + event.channel + ' :You didn\'t say who got told. Your told status: ' + told_status + '\n') 
         except TypeError:
