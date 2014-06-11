ed92cd1f6718c6273c4e052e494dd87a6dd25879
Matt Weller <hellmitre@gmail.com>
2014-06-11 15:24:24 -0700

    add .weather city state functionality

--- modules/weather.py
+++ modules/weather.py
@@ -9,7 +9,7 @@ class Weather:
     self.bot = bot
     self.say = say
 
-    self.help = ".weather <zipcode>"
+    self.help = ".weather <zipcode>, .weather <city> <state>"
 
     for event in events:
       if event._type in self.interests:
@@ -20,9 +20,27 @@ class Weather:
   def handle(self, event):
     _z = event.line.split()
     if _z[-1] != "":
+      #print event.msg.split()[1:]
       try:
-        zipcode = _z[-1] # zipcode should be last word on line (.weather 95928, eg)
-        url = 'http://api.wunderground.com/api/1fe31b3b4cfdab66/conditions/lang:EN/q/'+zipcode+'.json'
+        # for specifying by name
+        if len(event.msg.split()[1:]) > 1: # looking for .weather san francisco ca
+          autocomplete_url = 'http://autocomplete.wunderground.com/aq?query=' + " " .join(event.msg.split()[1:]) # should be everything after weather
+          response = urllib2.urlopen(autocomplete_url)
+          try:
+            j = response.read()
+          except urllib2.HTTPError:
+            self.printer("PRIVMSG " + event.channel + " :Wunderground appears down. Sorry\n")
+            return
+
+          parsed = json.loads(j)
+          zipcode = parsed['RESULTS'][0]['zmw']
+          url = 'http://api.wunderground.com/api/1fe31b3b4cfdab66/conditions/lang:EN/q/'+zipcode[:5]+'.json'
+
+        # by zipcode
+        else:
+          zipcode = _z[-1] # zipcode should be last word on line (.weather 95928, eg)
+          url = 'http://api.wunderground.com/api/1fe31b3b4cfdab66/conditions/lang:EN/q/'+zipcode+'.json'
+
         try:
           response = urllib2.urlopen(url)
         # catch timeouts
@@ -35,5 +53,6 @@ class Weather:
         #print json_string
         self.printer("PRIVMSG " + event.channel + " :" + parsed_json['current_observation']['display_location']['city'] + ", " + parsed_json['current_observation']['display_location']['state'] + ": " + parsed_json['current_observation']['weather'] + ", " + parsed_json['current_observation']['feelslike_string'] + ", " + parsed_json['current_observation']['relative_humidity'] +" humidity"+ '\n')
         #self.printer("PRIVMSG " + event.channel + " :For complete info: http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query="+zipcode + '\n')
-      except KeyError:
-        pass
+      except KeyError, e:
+        print "KeyError!"
+        print e
