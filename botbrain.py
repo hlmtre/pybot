1f032a41ad3e40db4f9780f154568066b5350a14
Matt Weller <hellmitre@gmail.com>
2014-09-19 16:15:04 -0700

    remove all teh old bf3 stuff

--- botbrain.py
+++ botbrain.py
@@ -1,10 +1,8 @@
 from collections import defaultdict
 import webwriter
-#import bf3api
 import time
 import logger 
 import datetime
-import battlelog
 import urllib2
 import json
 from urlparse import urlparse, parse_qsl
@@ -15,59 +13,6 @@ from datetime import datetime, timedelta
 import sys
 import os
 
-#api = bf3api.API(None, '360')
-#
-#bf3players = {'tarehart': ('tarehart', '360'),
-#              #'hlmtre': ('hellmitre', '360'),
-#              'willfro': ('fro4thought', '360'),
-#              'BoneKin': ('BoneKin', '360'),
-#              'Lucifer7': ('Lucifer7', '360'),
-#              'i7': ('Infinite Se7en', '360')}
-
-#def getbf3stats(message, platform):
-## global bf3players
-#  global api
-#
-#  gt = message.split(None, 2)[2].strip() # grab gamertag off the end and strip it of terminators
-#  command = message.split(None, 2)[1].strip()
-#
-#  data = api.player(gt, platform, "clear,ranking")
-#
-#  l = logger.Logger()
-#  date = str(time.strftime("%Y-%m-%d %H:%M:%S"))
-#  string = "getting " + command + " for player " + gt + " at " + date + '\n'
-#  l.write(string)
-#
-#  return formatbf3data(gt, command, data)
-#    
-#def formatbf3data(player, command, data):
-#  if data.status == "data":
-#    if command == "spm":
-#      return [player + "'s SPM: " + str(data.Stats.Ranking.Spm.v)[:6]]
-#    elif command == "kdr":
-#      return [player + "'s KDR: " + str(data.Stats.Ranking.Kdr.v)[:6]]
-#    elif command == "wlr":
-#      return [player + "'s WLR: " + str(data.Stats.Ranking.Wlr.v)[:6]]
-#    elif command == "stats":
-#      l = [player+"'s stats: ", "KDR: "+str(data.Stats.Ranking.Kdr.v)[:6], "SPM: "+str(data.Stats.Ranking.Spm.v)[:6], "Headshots/kill: "+str(data.Stats.Ranking.Hskillperc.v)[:6], "win/loss: "+str(data.Stats.Ranking.Wlr.v)[:6]]
-#      return l
-#      
-#  elif data.status == "notfound":
-#    return [player + " not found "]
-#  else:
-#    return ["ERROR"]
-#  
-#
-#def getbf3last(message):
-#  global api
-#  
-#  command = message.split(None, 2)[1].strip()
-#  gt = message.split(None, 2)[2].strip() # grab gamertag off the end and strip it of terminators
-#  fact = battlelog.getLatestGameFact(api, gt, command)
-#  return gt + "'s latest " + command + " is " + str(fact)[:6]
-
-  
-  
 class BotBrain:
   BRAINDEBUG = False
   
@@ -151,7 +96,6 @@ class BotBrain:
     self.say(channel, "Yep, I'm on. Idiot.")
 
   def _help(self, user):
-    #self.microphone('PRIVMSG ' + user + ' :' + ".bf3 [spm, kdr, wlr, stats],\n")
     self.microphone('PRIVMSG ' + user + ' :' + ".uptime,\n")
     self.microphone('PRIVMSG ' + user + ' :' + ".imgs,\n")
     self.microphone('PRIVMSG ' + user + ' :' + ".ctof [celsius],\n")
@@ -205,25 +149,6 @@ class BotBrain:
         self._ftoc(channel, last[-1]) 
     if message.startswith(".help"):
       self._help(usr)     
-    #if message.startswith(".bf3pc"):
-    #  stats = getbf3stats(message, 'pc')
-    #  try:
-    #    for line in stats:
-    #      self.say(channel, line)
-    #  except TypeError:
-    #    self.say(channel, "WOOPS")
-    #elif message.startswith(".bf3"):
-    #  stats = getbf3stats(message, '360')
-    #  try:
-    #    for line in stats:
-    #      self.say(channel, line)
-    #  except TypeError:
-    #    self.say(channel, "WOOPS")
-    #if message.startswith(".lastbf3"):
-    #  last = getbf3last(message)
-    #  self.say(channel, last)
-    #if message.startswith(".uptime"):
-    #  self._uptime(channel)
     if message.startswith(".onstat"):
       self._onstat(channel)
     if message.startswith(".speak"):
@@ -231,41 +156,3 @@ class BotBrain:
       chnl = tmp[1]
       msg = tmp[2]
       self._speak(usr, chnl, msg)
-
-# utility function
-#  def __prettyDate(self,time):
-#    now = datetime.now()
-#    if type(time) is int:
-#      diff = now - datetime.fromtimestamp(time)
-#    elif isinstance(time,datetime):
-#      diff = now - time 
-#    elif not time:
-#      diff = now - now
-#    second_diff = diff.seconds
-#    day_diff = diff.days
-#
-#    if day_diff < 0:
-#      return ''
-#
-#    if day_diff == 0:
-#      if second_diff < 10:
-#        return "just now"
-#      if second_diff < 60:
-#        return str(second_diff) + " seconds ago"
-#      if second_diff < 120:
-#        return  "a minute ago"
-#      if second_diff < 3600:
-#        return str( second_diff / 60 ) + " minutes ago"
-#      if second_diff < 7200:
-#        return "an hour ago"
-#      if second_diff < 86400:
-#        return str( second_diff / 3600 ) + " hours ago"
-#      if day_diff == 1:
-#        return "Yesterday"
-#      if day_diff < 7:
-#        return str(day_diff) + " days ago"
-#      if day_diff < 31:
-#        return str(day_diff/7) + " weeks ago"
-#      if day_diff < 365:
-#        return str(day_diff/30) + " months ago"
-#      return str(day_diff/365) + " years ago"
