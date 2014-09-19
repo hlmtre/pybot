faa14939f35cd95fdaa526732d527f3b351dbdd5
Matt Weller <hellmitre@gmail.com>
2014-09-19 10:04:18 -0700

    sphinx is ignoring my order to exclude :(

--- conf.py
+++ conf.py
@@ -75,7 +75,7 @@ release = pybot_version.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'modules/twitter_credentials.py']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
