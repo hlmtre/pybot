b3a85f18ff945a4758fea59d484964af36a3a873
Robert Harvey <the.kanbo@gmail.com>
2014-09-20 16:55:05 -0700

    Kellogs Git Crunch with Kanuggets

---
+++ modules/kanbomodule.py
@@ -0,0 +1,18 @@
+from event import Event
+try:
+  from basemodule import BaseModule
+except ImportError:
+  from modules.basemodule import BaseModule
+class KanboModule(BaseModule):
+  def post_init(self):
+    pass
+    #custom = Event("__.custom__")
+    #custom.define(msg_definition="^\.custom")
+    #custom.subscribe(self)
+
+    # register ourself to our new custom event
+    #self.bot.register_event(custom, self)
+    
+  def handle(self, event):
+    pass
+    #self.say(event.channel, "custom event caught!")
