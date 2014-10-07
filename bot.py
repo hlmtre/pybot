9984291717b28c335a9bc7f60456e004738b9b68
Matt Weller <hellmitre@gmail.com>
2014-10-06 17:21:46 -0700

    add setting DEBUG to true if not hasattr(os,'fork')

--- bot.py
+++ bot.py
@@ -497,6 +497,7 @@ if __name__ == "__main__":
     elif pid > 0:
       sys.exit(0)
   else: # don't background; either we're in debug (foreground) mode, or on windows TODO
+    DEBUG = True
     print "starting bot, pid " + util.bcolors.GREEN + str(os.getpid()) + util.bcolors.ENDC
     if len(sys.argv) > 1 and sys.argv[1] != "-d": # the conf file must be first argument
       CONF = sys.argv[1]
