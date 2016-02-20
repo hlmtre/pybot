feffe1580a7c7728005e97ffac881c23f3732d78
Matt Weller <hellmitre@gmail.com>
2016-02-20 13:29:14 -0800

    add requirements and v0.8.6.1

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
   name = 'yapybot',
   packages = ['yapybot'],
-  version = '0.8.6',
+  version = '0.8.6.1',
   description = 'yet another python irc bot',
   author = 'Matt Weller',
   author_email = 'hellmitre@gmail.com',
   url = 'https://github.com/hlmtre/pybot',
-  download_url = 'https://github.com/hlmtre/pybot/tarball/v0.8.5',
+  download_url = 'https://github.com/hlmtre/pybot/tarball/v0.8.6.1',
   keywords = ['irc', 'bot', 'modular'],
+  install_requires = ['praw', 'mysql-python', 'python-twitter', 'imgurpython', 'isodate'],
   classifiers = [],
 )
 
