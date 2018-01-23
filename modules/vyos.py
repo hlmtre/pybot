257cedf85e63151c3693833ede748c3b4c1c6fb1
Matt Weller <hellmitre@gmail.com>
2018-01-23 14:30:33 -0800

    change to lowercase for vyos name

--- modules/vyos.py
+++ modules/vyos.py
@@ -9,7 +9,7 @@ class Vyos(BaseModule):
     vevent.define(msg_definition="^\.vyos")
     vevent.subscribe(self)
 
-    self.box_to_ip = dict([('mechznc', '10.0.0.76'), ('BoneKin', '192.168.17.40'), ('thraust', '192.168.0.126')])
+    self.box_to_ip = dict([('mech', '10.0.0.76'), ('bonekin', '192.168.17.40'), ('thraust', '192.168.0.126')])
 
     self.bot.register_event(vevent, self)
   
@@ -25,7 +25,7 @@ class Vyos(BaseModule):
   def ping(self, nick):
     import subprocess
     try:
-      resp = subprocess.call(["ping", "-c 1", self.box_to_ip[nick]])
+      resp = subprocess.call(["ping", "-c 1", self.box_to_ip[nick.lower()]])
     except KeyError:
       self.say(self.event.channel, "BAD NAME IDIOT")
       return False
