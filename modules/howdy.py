5d62f5649f8b0923d8ba27c6f57f7fe9ad5a0a87
Matt Weller <hellmitre@gmail.com>
2018-12-16 09:44:55 -0800

    re-add howdy

---
+++ modules/howdy.py
@@ -0,0 +1,17 @@
+from event import Event
+import random
+try:
+  from basemodule import BaseModule
+except ImportError:
+  from modules.basemodule import BaseModule
+class Howdy(BaseModule):
+  def post_init(self):
+    howdy = Event("__.howdy__")
+    howdy.define(msg_definition="^\.howdy")
+    howdy.subscribe(self)
+
+    # register ourself to our new howdy event
+    self.bot.register_event(howdy, self)
+    
+  def handle(self, event):
+    self.say(event.channel, "https://i.imgur.com/veDWwOv.gif")
