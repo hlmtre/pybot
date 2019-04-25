902b56e6588adbe421db7a16070cb6f05e92a306
MechMaster48 <zfoster777@gmail.com>
2019-04-25 09:20:29 -0700

    tell module moved over to use basemodule along with some added error handling to give the user some sort of message when they have used the module command wrong...also to harrass them a bit. Tell has also been removed completely from bot.py since basemodule has taken over

--- bot.py
+++ bot.py
@@ -109,9 +109,6 @@ class Bot(threading.Thread):
     part = Event("__.part__")
     part.define("part")
 
-    tell = Event("__privmsg__")
-    tell.define("PRIVMSG")
-
     links = Event("__urls__")
     links.define("https?://*")
 
@@ -127,7 +124,6 @@ class Bot(threading.Thread):
     self.events_list.append(weather)
     self.events_list.append(steam)
     self.events_list.append(part)
-    self.events_list.append(tell)
     self.events_list.append(links)
     #self.events_list.append(unloads)
   # example
