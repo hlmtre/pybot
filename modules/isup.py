ebeb0ef552136c11051d96c0344bd3d5ec958a91
MechMaster48 <zfoster777@gmail.com>
2019-05-11 13:03:18 -0700

    Reworked the module by doing a get request on a URL that would just output JSON data and pulling the data from that without all the messy javascript on the main page

--- modules/isup.py
+++ modules/isup.py
@@ -1,39 +1,48 @@
+##Isup module created by hlmtre##
+
 from event import Event
 try:
   import requests
 except ImportError:
-  print "Warning: isup module requires requests."
+  print "Warning: isup module requires requests"
   requests = object
-from xml.dom.minidom import parseString
+import json
 try:
   from basemodule import BaseModule
 except ImportError:
   from modules.basemodule import BaseModule
+
 class Isup(BaseModule):
   def post_init(self):
     isup = Event("__.isup__")
     isup.define(msg_definition="^\.isup")
     isup.subscribe(self)
-
+    self.help = ".isup <Valid website using *.com, *.net, etc.>"
+    
     # register ourself to our new isup event
     self.bot.register_event(isup, self)
 
-    self.url = "http://isup.me/"
-    
+    self.url = "https://api.downfor.cloud/httpcheck/" # URL which outputs JSON data
+
+    """
+    Example to show json data parameters that can be pulled from with current URL get request:
+
+    statusCode	200
+    statusText	"OK"
+    isDown	false
+    returnedUrl	"https://www.reddit.com/"
+    requestedDomain	"reddit.com"
+    lastChecked	1557603603861
+    """    
   def handle(self, event):
-    if len(event.msg.split()) == 2:
+    if len(event.msg.split()) == 2: # Looks for the command and hopefully a valid website (*.com,*.net, etc.)
       try:
-        r = requests.get(self.url + event.msg.split()[1])
+        r = requests.get(self.url + event.msg.split()[1]) # Takes our static URL and appends your site to the end to make our get request
+        j = json.loads(r.text) # Converts our JSON to python object
+        if str(j["isDown"]) == "True": # Converts our parameter to a string to compare against our "isDown" parameter
+          self.say(event.channel, "Site looks down, its not just you") # Once state is determined it will be spit out into the channel
+        elif str(j["isDown"]) == "False":
+          self.say(event.channel, "Site looks ok to me, its just you")
+        
       except requests.ConnectionError:
         self.say("Connection error.")
-      # we get back plain HTML. hopefully the phrases don't change.
-      up = "It's just you."
-      down = "looks down from here"
-      not_a_site = "doesn't look like a site"
-      if r.text.find(up) != -1:
-        self.say(event.channel, event.msg.split()[1] + " looks up.")
-      elif r.text.find(not_a_site) != -1:
-        self.say(event.channel, event.msg.split()[1] + " is not a site.")
-      elif r.text.find(down) != -1:
-        self.say(event.channel, event.msg.split()[1] + " looks down.")
-        
