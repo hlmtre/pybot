c519565bd174c6821a9380ca446c1aebe43616f7
Matt Weller <hellmitre@gmail.com>
2019-05-11 14:51:10 -0700

    small formatting/grammar fixes in isup

--- modules/isup.py
+++ modules/isup.py
@@ -1,4 +1,5 @@
-##Isup module created by hlmtre##
+## version 0.1 created by hlmtre ##
+## version 0.2 updated by mech ##
 
 from event import Event
 try:
@@ -18,7 +19,7 @@ class Isup(BaseModule):
     isup.define(msg_definition="^\.isup")
     isup.subscribe(self)
     self.help = ".isup <Valid website using *.com, *.net, etc.>"
-    
+
     # register ourself to our new isup event
     self.bot.register_event(isup, self)
 
@@ -33,16 +34,16 @@ class Isup(BaseModule):
     returnedUrl	"https://www.reddit.com/"
     requestedDomain	"reddit.com"
     lastChecked	1557603603861
-    """    
+    """
   def handle(self, event):
     if len(event.msg.split()) == 2: # Looks for the command and hopefully a valid website (*.com,*.net, etc.)
       try:
         r = requests.get(self.url + event.msg.split()[1]) # Takes our static URL and appends your site to the end to make our get request
         j = json.loads(r.text) # Converts our JSON to python object
         if str(j["isDown"]) == "True": # Converts our parameter to a string to compare against our "isDown" parameter
-          self.say(event.channel, "Site looks down, its not just you") # Once state is determined it will be spit out into the channel
+          self.say(event.channel, "Site looks down; it's not just you.") # Once state is determined it will be spit out into the channel
         elif str(j["isDown"]) == "False":
-          self.say(event.channel, "Site looks ok to me, its just you")
-        
+          self.say(event.channel, "Site looks ok to me; it's just you.")
+
       except requests.ConnectionError:
         self.say("Connection error.")
