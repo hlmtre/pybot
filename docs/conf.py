10de2f90d9f3339a6a801aaaaa3759d4f0cec43c
Matt Weller <hellmitre@gmail.com>
2019-12-20 22:38:49 -0800

    DOX PLS

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, '/home/hlmtre/pybot')
+import os
+import sys
+sys.path.insert(0, '/home/hlmtre/pybot')
+sys.path.insert(0, '/home/hlmtre/pybot/modules')
+sys.path.insert(0, '/home/hlmtre/pybot/modules/snippets')
 
 
 # -- Project information -----------------------------------------------------
