2f2a7f24c150cdc7ed1f981aca55ad55ae261dd0
Matt Weller <hellmitre@gmail.com>
2020-07-09 15:55:51 -0700

    add totally useful and meaningful pieces of code in definitely grownup modules

--- modules/snippets/downtime.py
+++ modules/snippets/downtime.py
@@ -4,7 +4,7 @@ from util import commands, parse_line
 
 @commands(".downtime")
 def _downtime(bot, message, channel):
-  drinks = ['a beer', 'a scotch', 'a bloody mary', 'a nice glass of wine', 'FUCKIN FOUR LOKO', 'a crisp cider']
+  drinks = ['a beer', 'a scotch', 'a bloody mary', 'a nice glass of wine', 'FUCKIN FOUR LOKO', 'a crisp cider', 'a margarita from BoneKin and Jennos\'s house']
   action_string = "\001ACTION "
   user = parse_line(message).user
   if user.lower() == "george" or "thorogood" in user.lower():
---
+++ modules/snippets/shrug.py
@@ -0,0 +1,6 @@
+from random import randint
+from util import commands
+
+@commands(".shrug")
+def shrug(bot, message, channel):
+  bot.say(channel, "¯\_(ツ)_/¯")
